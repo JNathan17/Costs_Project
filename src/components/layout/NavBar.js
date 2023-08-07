e05d41cd6c631cdbc82b282f757c32479ab11db7
jNathan17 <jnathantavares17@gmail.com>
2023-08-07 10:31:27 -0300

    inserindo dados no banco

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -8,12 +8,12 @@ import logo from '../../img/costs_logo.png'
 function NavBar(){
     return(
         <div>
-            <nav class={styles.navbar}>
+            <nav className={styles.navbar}>
                 <Container>
                     <Link to="/">
                         <img src={logo} alt="Costs"></img>
                     </Link>
-                    <ul class = {styles.list}>
+                    <ul className = {styles.list}>
                         <li className = {styles.item}>
                             <Link to="/">Home</Link>
                         </li>
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,39 @@
+import { useNavigate} from 'react-router-dom'
+
 import ProjectForm from '../Project/ProjectForm'
 import styles from './NewProject.module.css'
 
 function NewProject(){
+
+    const navigate = useNavigate()
+
+    function createPost(project){
+
+        //iniciaçao cost e services
+        project.cost = 0
+        project.services = []
+
+        fetch('http://localhost:5000/projects',{
+            method: "POST",
+            headers:{
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project)//para enviar como string 
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data)
+            navigate('/projects', {message: 'projeto criado com sucesso!'})
+        })
+        .catch(err => console.log(err))
+
+    }
+
     return(
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para adicionar serviços</p>
-            <ProjectForm btnText="Criar Projeto" />
+            <ProjectForm  handleSubmit={createPost} btnText="Criar Projeto" />
         </div>
     )
 }
